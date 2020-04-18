1bec4889d5a4c944cf9477a91287d9bb9a5889a0
BromTeque <aleksander@lingstad.no>
2020-04-18 12:27:44 +0200

    Added more failure states and some checks

--- word.exe.bat
+++ word.exe.bat
@@ -1,12 +1,8 @@
 @ECHO OFF
 REM TODO Add more failure/error states/handlers.
 
-
-
 REM Change "root" to Riot Vanguard install location
 SET root=C:\Program Files\Riot Vanguard
-
-
 
 REM Detect if script is run as admin. If it itsn't it pauses and then quits.
 NET SESSION >nul 2>&1
@@ -17,9 +13,47 @@ IF %ERRORLEVEL% EQU 0 (
 	ECHO This script must be run as administrator to work properly! 
 	ECHO Right click -^> Run as administrator to fix.
 	ECHO.
-	PAUSE
+	PAUSE >nul
 	EXIT /B 1
 )
+
+REM Checks if Riot Vanguard is installed in default location, then goes to Riot Vanguard Location.
+IF EXIST "%root%\vgc.ico" (
+	CD %root%
+) ELSE (
+	ECHO Riot Vanguard not found in default location. Edit bat file to fix.
+	PAUSE >nul
+	EXIT /B 1
+)
+
+REM Tells user if vgk.sys is running or not, and if vgk.sys is renamed or not.
+ECHO.
+FOR /F "tokens=3 delims=: " %%H IN ('sc query vgk ^| findstr "        STATE"') DO (
+	IF /I "%%H" EQU "RUNNING" (
+		IF EXIST vgk.sys (
+			ECHO Riot Vanguard running
+			ECHO.
+			ECHO Press any key to disable Riot Vanguard...
+		) ELSE (
+			ECHO Riot Vanguard already disabled. Restart your PC for changes to take effect.
+			PAUSE >nul
+			EXIT /B 1
+		)
+		
+	) ELSE (
+		IF EXIST vgk.sys (
+			ECHO Riot Vanguard already enabled. Restart your PC for changes to take effect.
+			PAUSE >nul
+			EXIT /B 1
+		) ELSE (
+			ECHO VGK Not Running
+			ECHO.
+			ECHO Press any key to Enable VGK...
+		)
+	)
+)
+PAUSE >nul
+CLS
 
 REM Informs user that the script will reboot their PC. Then pauses until user continues.
 ECHO.
